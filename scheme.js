1e8b8878a6379094ed1bde4527acd81602959c1d
Travis Hoover <thoov7@gmail.com>
2012-09-24 18:39:14 -0700

    Rechanging logic for eval

--- scheme.js
+++ scheme.js
@@ -1,143 +1,226 @@
  // Author: Travis Hoover, thoov7@gmail.com
 
-//
-// Look up and see if a token identifier has previously been defined.
-//
-var STANDARD_FUNCTIONS = ["DEFINE", "+", "-", "*", "/"];
-var standardLibLookup = function ( token ) {
 
-    if( STANDARD_FUNCTIONS.indexOf( token ) !== -1 ) return token;
-    return false; 
+//
+// Alist is the symbol table.
+//
+// Holds all of the symbols and their values along with primative functions.
+//
+var alist = { type:'NULL' };
+var primfns = [];
+
+//
+// Make functions for the alist
+//
+var makeNumber = function ( number ) {
+
+    return { type:'NUMBER', val:number };
 }
-//
-// Look up and see if a token identifier has previously been defined.
-//
-var VARIABLES = [];
-var variableLookup = function ( token ) {
-
-    if ( VARIABLES[token] !== null ) return VARIABLES[token];
-    return false;
+var makeNull = function () {
+    
+    return { type:'NULL' };
+}
+var makeSymbol = function( symbol ) {
+    
+    return { type:'SYMBOL', val:symbol };    
+}
+var makeString = function( string ) {
+    
+    return { type:'STRING', val:string };
+}
+var makeCons = function ( car, cdr ) {
+    
+    return { type:'CONS', car:car, cdr:cdr };
+}
+var makePrimFunction = function ( number ) {
+    return { type:'PRIM', val:number };
 }
 
+var makeElement = function (type, value) {
+    return { type:type, val:value};
+}
+var appendAlist = function (type, value) {
+    
+    alist = makeCons( makeCons(makeSymbol('PI'), makeNumber(3.14)),  alist);
+}
+
+//
+// load the primative functions into the alist.
+//
+alist = makeCons( makeCons(makeSymbol('def'), makePrimFunction(1)),  alist);
+alist = makeCons( makeCons(makeSymbol('+'), makePrimFunction(2)),  alist);
+alist = makeCons( makeCons(makeSymbol('-'), makePrimFunction(3)),  alist);
+alist = makeCons( makeCons(makeSymbol('PI'), makeNumber(3.14)),  alist);
 
 
 //
-// Predefined constants such as pi.
+// Prim function for def
 //
-VARIABLES["PI"] = 3.14;
-
-
-
-
-
-
-var SYMBOL = "SYMBOL";
-var LIST = "LIST";
-
-
-
+// TODO: make sure correct types.
 //
-// Go through the list and turn identifiers into values.
-// Keep standard function identifiers and handle the special case of define.
-//
-var standardize = function ( tokenized ) { 
+primfns[1] = function (sexpr) {
+    
+    var def = sexpr[0]; // The def keyword is the first token on the stack.
 
-    if ( tokenized.type.toUpperCase() === LIST ) {
+    var variable  = sexpr[1]; // The second token is the variable being defined.
+
+
+    //
+    // The third param is the value for the variable
+    // It can either be an atom or a sub list. We eval it to get a value.
+    //
+    var value = eval(sexpr[2]);
+    
+    
+    //
+    // Insert into alist the new variable that was defined.
+    //
+    if (typeof value === 'number') {
         
-        var array = [];
-
-        // move down the stack and 'pop' the list off of the stack
-        for ( var i = 0; i < tokenized.val.length; i++ ) {
-            
-            //
-            //  If the define symbol is first then the next token is special so dont eval it.
-            //
-            if ( tokenized.val[0].val.toUpperCase() == "DEFINE" && i == 1 ) array.push(tokenized.val[i]); 
-            else array.push(standardize( tokenized.val[i] ));    
-        }
-                
-        return array;
+        alist = makeCons( makeCons(variable, makeNumber(value)),  alist);   
     }
-    else if ( tokenized.type.toUpperCase() === SYMBOL ) {
-        
-        //
-        // Check to see if the identifier is a standard library or variable.
-        //
-        if ( standardLibLookup(tokenized.val.toUpperCase()) ) return standardLibLookup(tokenized.val.toUpperCase());
-        else if ( variableLookup(tokenized.val.toUpperCase().trim()) ) return variableLookup(tokenized.val.toUpperCase());
-        else {
-            console.log("Unidentified identifier: " + tokenized.val);
-            return "ERROR";
-        }
+    else if (typeof value === 'string') {
+    
+        alist = makeCons( makeCons(variable, makeString(value)),  alist);
     }
     else {
         
-        console.log("The token we are looking at is either a string or a number: " + tokenized.val);
-        return tokenized.val;
-    } 
+        console.log("Feature not added yet.");
+    }
+    
+    return true;
+}
+
+//
+// Prim function for +
+//
+primfns[2] = function (sexpr) {
+
+    var plus = sexpr[0]; // The plus sign
+
+    var value = eval(sexpr[1]); // The first element after the + sign goes on the left hand side of the plus sign.
+
+
+    //
+    // Loop through the rest of the elments and add them up.
+    //
+    for (var i = 2; i < sexpr.length; i++) {
+        
+        value += eval(sexpr[i]);
+    }
+    
+    return value;
 }
 
 
-var eval = function ( standardized ) {
-    
-    var opperation = standardized[0];
-    var result = null;
-    
-    
-    for ( var i = 1; i < standardized.length; i++ ) {
-       
-        if ( i === 1 ) {
-            result = standardized[i];
+
+
+//
+// Loopup symbols inside of the alist.
+//
+// @param symbol - The symbol to find inside of the alist.
+// @return - { type:'PRIM', val:1 } or { type:'NUMBER', val:23} or false if not found.
+//
+var lookup = function (symbol) {
+
+    var alistPtr = alist;
+        
+    while (alistPtr.type !== 'NULL') {
+        
+        if (alistPtr.car.car.val == symbol.val) {
+
+            // The return value is like { type:'PRIM', val:1 } or { type:'NUMBER', val:23}
+            return alistPtr.car.cdr;
         }
         else {
-       
-            if( standardized[i] instanceof Array ) {
-                
-                if( opperation == "+") {
-                    result += eval(standardized[i]);
-                }
-                else if (opperation == "-"){
-                    result -= eval(standardized[i]);
-                }
-                else if(opperation == "*") {
-                    result *= eval(standardized[i]);
-                }
-                else if (opperation == "/") {
-                    result /= eval(standardized[i]);
-                }
-            }
-            else {
-                if( opperation == "+") {
-                    result += standardized[i];
-                }
-                else if (opperation == "-"){
-                    result -= standardized[i];
-                }
-                else if(opperation == "*") {
-                    result *= standardized[i];
-                }
-                else if (opperation == "/") {
-                    result /= standardized[i];
-                }   
-            }
-        
+            
+            alistPtr = alistPtr.cdr; // "advance the pointer"
+
+            // If null then we are at the end of the alist.
+            if (alistPtr.type === 'NULL') { alistPtr = { type:'NULL' }; } 
         }
     }
     
-    return result;
+    return false;
 }
 
+var listSymbols = function () {
+    
+    var alistPtr = alist;
+    
+    while (alistPtr.type !== 'NULL') {
+    
+        console.log(alistPtr.car.car.val);
+                
+        alistPtr = alistPtr.cdr; // "advance the pointer"
+
+        // If null then we are at the end of the alist.
+        if (alistPtr.type === 'NULL') { alistPtr = { type:'NULL' }; } 
+        
+    }
+
+}
+
+
+
+
+//
+// Evaluation function. Pops off sexpr from a tokenized stack of sexpr.
+//
+// @param SEXPR - A stack of sexpr expressions to be evaluated.
+// @param RETURN - Return the value of an individual token.
+//
+var eval = function( SEXPR ) {
+
+    var currentSexpr = SEXPR;
+    var parentExpression = SEXPR;
+
+    //
+    // If the sexpr is a list then we want to eval the array.
+    //
+    if (SEXPR.type === 'LIST') {
+        currentSexpr = SEXPR.val[0];
+        parentExpression = SEXPR.val;
+    }
+
+
+    if (currentSexpr.type === 'SYMBOL') {
+        
+        var lookupValue = lookup( currentSexpr );
+        
+        if (lookupValue.type === 'PRIM') {
+             
+            return primfns[lookupValue.val]( parentExpression );
+        }
+        else if (lookupValue.type !== 'NULL') {
+            
+            return lookupValue.val;
+        }
+        else {
+            
+            // Error invalid symbol not defined.  
+            process.exit(1); 
+        }
+    }
+    else if(currentSexpr.type === 'LIST') {
+        
+        return eval(currentSexpr.val);
+    }
+    else { // Numbers and strings
+
+        return currentSexpr.val;
+    }
+}
 
 
 
 
 
 var tokenized = null;
-
 var createItem = function (identifier, value) { return {type:identifier.toUpperCase, val:value}; };
 var create_null = function () { return { type:'NULL' }; }
 var create_string = function (x) { return { type:'STRING', val:x }; }
-var create_symbol = function (x) { return { type:SYMBOL, val:x }; }
+var create_symbol = function (x) { return { type:'SYMBOL', val:x }; }
 var create_number = function (x) { return { type:"NUMBER", val:x }; }
 var create_list = function (x) { 
     
@@ -377,6 +460,12 @@ switch( match )
 	case 5:
 		{
 		 info.att = parseFloat( info.att ); 
+		}
+		break;
+
+	case 6:
+		{
+		 /*symbols cannot begin with digits, parens, quotes, dots, etc.*/ 
 		}
 		break;
 
@@ -771,33 +860,35 @@ process.stdin.on('data', function (text) {
 
     // ( R )
     
-    if( text.trim() != "" ) {
+    text = text.trim();
     
-        // send text to scheme tokenizer ( E )
-        if( ( error_count = __NODEJS_parse( text.trim(), error_offsets, error_lookaheads ) ) > 0 ) {
-       
-            for( var i = 0; i < error_count; i++ )
-                console.log( "Parse error near " + text.substr( error_offsets[i] ) + ", expecting \"" + error_lookaheads[i].join() + "\"" );
+    if( text != "" ) {
+        
+                    
+        if (text.toLowerCase() == 'print alist') {
+            
+            listSymbols();
         }
         else {
-      
-            if (DEBUG) { 
+        
+            // send text to scheme tokenizer ( E )
+            if( ( error_count = __NODEJS_parse( text, error_offsets, error_lookaheads ) ) > 0 ) {
+           
+                for( var i = 0; i < error_count; i++ )
+                    console.log( "Parse error near " + text.substr( error_offsets[i] ) + ", expecting \"" + error_lookaheads[i].join() + "\"" );
+            }
+            else {
+          
+                if (DEBUG) { 
+                    
+                    console.log('> Tokenized: ');
+                    console.log(JSON.stringify(tokenized)); 
+                    console.log('> Eval: ');
+                    
                 
-                console.log('> Tokenized: ');
-                console.log('> ' + JSON.stringify(tokenized)); 
-                
-                console.log('> ');
-                console.log('> Standardized: ');
-                var standard = standardize(tokenized)
-                console.log(standard);
-                
-                console.log('> ');
-                console.log('> Eval: ');
-                console.log(eval(standard));
-                //console.log(tokenized);
-            
-            } // print the internal json structure
-            process.stdout.write(text); // print the read stage text back as the print stage ( P )
+                }
+                console.log(eval(tokenized)); // Print the evaluated expression ( P )
+            }
         }
     }
     process.stdout.write("> "); // now loop or wait for user input again ( L )
